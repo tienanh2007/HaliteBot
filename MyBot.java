b4398df4fb844d645431b4343b8178353ed4775d
Tien Anh Nguyen <nguyentienanh68@gmail.com>
2016-11-13 23:15:32 -0500

    Add files via upload

--- MyBot.java
+++ MyBot.java
@@ -22,30 +22,40 @@ public class MyBot {
 				for(int x = 0; x < gameMap.width; x++) {
 					Site site = gameMap.getSite(new Location(x, y));
 					if(site.owner == myID) {
+						Location loc = new Location(x, y);
 						Direction dir = Direction.randomDirection();
 						Direction min = min(gameMap,x,y, myID);
-						int ourPro = gameMap.getSite(new Location(x, y)).production;
-						int ourStr = gameMap.getSite(new Location(x,y)).strength;
+						int ourPro = gameMap.getSite(loc).production;
+						int ourStr = gameMap.getSite(loc).strength;
 						Direction minLen = minLen(gameMap, x, y, myID);
+						Direction maxPro = hiP(gameMap, x, y, myID);
+						int maxProStr = (gameMap.getSite(loc, maxPro).strength);
+						int minPro = (gameMap.getSite(loc, min).strength);
 						if(ourStr == 0){
-							moves.add(new Move(new Location(x,y),Direction.STILL));
+							moves.add(new Move(loc,Direction.STILL));
+						}
+						else if(ourPro != 0 && maxPro != null && maxProStr/ourPro <= 2*minPro/ourPro){
+							if(gameMap.getSite(loc,maxPro).strength < ourStr)
+								moves.add(new Move(loc, maxPro));
+							else 
+								moves.add(new Move(loc,Direction.STILL));
 						}
 						else if(min != null){
-							if(gameMap.getSite(new Location(x, y),min).strength < ourStr)
-								moves.add(new Move(new Location(x, y), min));
+							if(gameMap.getSite(loc,min).strength < ourStr)
+								moves.add(new Move(loc, min));
 							else 
-								moves.add(new Move(new Location(x,y),Direction.STILL));
+								moves.add(new Move(loc,Direction.STILL));
 						}
 						else if(minLen != null){
 							if(ourStr < ourPro * 5 ){
-								moves.add(new Move(new Location(x,y),Direction.STILL));
+								moves.add(new Move(loc,Direction.STILL));
 							}
 							else{
-								moves.add(new Move(new Location(x, y), minLen));
+								moves.add(new Move(loc, minLen));
 							}
 						}
 						else{
-							moves.add(new Move(new Location(x, y), Direction.randomDirection()));
+							moves.add(new Move(loc, Direction.randomDirection()));
 						}
 					}
 				}
@@ -91,5 +101,37 @@ public class MyBot {
 			}
 		}
 		return minDir;
+	}
+	public static Direction hiP(GameMap map, int x, int y, int myID){
+		ArrayList<Direction> directions = new ArrayList<>();
+		ArrayList<Integer> productions = new ArrayList<>();
+		for(int i=0;i<dirs.length;i++){
+			if(map.getSite(new Location(x, y), dirs[i]).owner != myID){
+				directions.add(dirs[i]);
+			}
+		}
+		for(int i=0;i<directions.size();i++){
+			Location curL = new Location(x, y);
+			productions.add(0);
+			for(int j=0;j<5;j++){
+				curL = map.getLocation(curL, directions.get(i));
+				int p = productions.get(i)+map.getSite(curL).strength;
+				if(directions.get(i) != Direction.WEST && directions.get(i) != Direction.EAST){
+					if(map.getSite(curL, Direction.WEST).owner != myID) 
+						p += map.getSite(curL, Direction.WEST).strength;
+					if(map.getSite(curL, Direction.EAST).owner != myID) 
+						p += map.getSite(curL, Direction.EAST).strength;
+				}
+				else{
+					if(map.getSite(curL, Direction.NORTH).owner != myID) 
+						p += map.getSite(curL, Direction.NORTH).strength;
+					if(map.getSite(curL, Direction.SOUTH).owner != myID) 
+						p += map.getSite(curL, Direction.SOUTH).strength;
+				}
+				productions.set(i, p);
+			}
+		}
+		if(directions.isEmpty()) return null;
+		return directions.get(productions.indexOf(Collections.max(productions)));
 	}
 }
